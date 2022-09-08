64d679c6d6421dc92f0b752a64c18470c82933c7
Andrii Biakov <an.bjakov@gmail.com>
2022-09-08 13:53:33 +0300

    rename makeApi, change appendListItem and so on

--- index.js
+++ index.js
@@ -2,24 +2,20 @@
 const API_URL = "https://jsonplaceholder.typicode.com/albums";
 const albumsList = document.getElementById('albums');
 
-makeApiRequest(API_URL).then(resolve=>{
-    resolve.map(album=>appendListItem(albumsList,album.title));
-    addListItemClass(albumsList,'album_title');
+fetchAPI(API_URL).then(albums=>{
+    albums.map(album=>appendListItem(albumsList,album.title));
 })
 
-function addListItemClass(targetList,className){
-    const itemsArray = Array.from(targetList.querySelectorAll('li'));
-    itemsArray.map(item=>item.classList.add(className))
-}
+
 
 function appendListItem(targetList,content){
-    const doc = document;
-    const listNode = doc.createElement('li');
+    const listNode = document.createElement('li');
+    listNode.classList.add('album_title');
     listNode.innerHTML = content;
     targetList.append(listNode);
 }
 
-async function makeApiRequest(API_URL){
+async function fetchAPI (API_URL){
     const response = await fetch(API_URL);
     const resolve = await response.json();
     try{
